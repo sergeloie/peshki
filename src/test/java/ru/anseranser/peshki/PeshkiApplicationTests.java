b1ecdf828ce47b4758a3cfa4ee7c45ede54367c8
admin <serge.loie@yandex.ru>
2026-06-14 18:16:02 +0500

    rework classes

--- src/test/java/ru/anseranser/peshki/PeshkiApplicationTests.java
+++ src/test/java/ru/anseranser/peshki/PeshkiApplicationTests.java
@@ -1,20 +1,5 @@
 package ru.anseranser.peshki;
 
-import org.junit.jupiter.api.Test;
-
-
 class PeshkiApplicationTests {
-
-
-	@Test
-	void contextLoads() {
-		for (int i = 0; i < 27; i++) {
-			System.out.print(getRelativePosition(i, 21) + " ");
-		}
-	}
-
-	public int getRelativePosition(int absolutePosition, int offset) {
-		return (absolutePosition - offset + 28) % 28;
-	}
 
 }
