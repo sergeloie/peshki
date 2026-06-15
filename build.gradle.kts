e072b6404d2343cfab4b2f511cac146a30c45ab4
admin <serge.loie@yandex.ru>
2026-06-15 20:05:45 +0500

    rework classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
 	application
 	alias(libs.plugins.freefairLombokPLugin)
-	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
 	id("com.autonomousapps.dependency-analysis") version "1.30.0"
 }
 
@@ -30,20 +28,13 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-
-	implementation("org.atp-fivt:ljv:1.04")
-
 	compileOnly(libs.lombok)
 	annotationProcessor(libs.lombok)
 	testCompileOnly(libs.lombok)
 	testAnnotationProcessor(libs.lombok)
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-	implementation("com.lihaoyi:mill-moduledefs_3:0.12.7")
 }
 
 tasks.withType<Test> {
--- src/main/java/ru/anseranser/peshki/Main.java
+++ src/main/java/ru/anseranser/peshki/Main.java
@@ -1,10 +1,50 @@
 package ru.anseranser.peshki;
 
-import ru.anseranser.peshki.input.ConsoleMoveInput;
+import ru.anseranser.peshki.engine.GameConfig;
+import ru.anseranser.peshki.engine.GameEngine;
+import ru.anseranser.peshki.engine.Player;
+import ru.anseranser.peshki.engine.event.GameEvent;
+import ru.anseranser.peshki.ui.console.ConsoleInput;
+import ru.anseranser.peshki.ui.console.ConsoleOutput;
+
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
-        Game game = new Game(new ConsoleMoveInput());
-        game.start();
+        GameConfig config = GameConfig.DEFAULT;
+        GameEngine engine = new GameEngine(config);
+        ConsoleOutput output = new ConsoleOutput();
+        ConsoleInput input = new ConsoleInput();
+
+        int turnCount = 0;
+        while (!engine.isGameOver() && turnCount < config.maxTurns()) {
+            turnCount++;
+            Player currentPlayer = engine.getBoard().getPlayers().get(engine.getCurrentPlayerIndex());
+
+            output.onTurnHeader(turnCount, currentPlayer.getNumber(), currentPlayer.isHuman());
+
+            List<Integer> dice = engine.rollDice();
+            output.onEvents(List.of(new GameEvent.DiceRolled(currentPlayer.getNumber(), dice)));
+
+            boolean extraTurn;
+
+            if (currentPlayer.isHuman()) {
+                extraTurn = engine.executeHumanTurn(input, output);
+            } else {
+                List<GameEvent> events = engine.executeBotTurn();
+                output.onEvents(events);
+                extraTurn = events.stream().anyMatch(e ->
+                        e instanceof GameEvent.TurnEnded te && te.extraTurn());
+                output.onBoard(engine.getBoard());
+            }
+
+            if (engine.isGameOver()) {
+                Player winner = engine.getBoard().getPlayers().get(engine.getCurrentPlayerIndex());
+                output.onGameWon(winner.getNumber());
+                return;
+            }
+        }
+        output.onGameEnded(config.maxTurns());
     }
 }
